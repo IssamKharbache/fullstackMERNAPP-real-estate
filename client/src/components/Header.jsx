1623c5f2b427c3f2e5e2f6bc8c4684af259187dd
lefline <issamkharbache2@gmail.com>
2023-10-14 10:50:10 +0100

    create and run express server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,10 +2,10 @@ import {FaSearch} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-lg'>
+    <header className='bg-blue-400 shadow-lg'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'><h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-      <span className='text-slate-500'>Kharbache</span>
+      <span className='text-white'>Kharbache</span>
       <span className='text-slate-700'>Esatate</span>
     </h1></Link>
         
@@ -16,9 +16,9 @@ className='bg-transparent focus:outline-none w-24 sm:w-64 md:54' />
 <FaSearch className='text-slate-600' />
     </form>
     <ul className='flex gap-4 '>
-      <Link to='/'><li className='hidden sm:inline text-blue-950 hover:text-slate-400'>Home</li></Link>
-      <Link to='/about'><li className='hidden sm:inline text-blue-950 hover:text-slate-400' >About</li></Link>
-      <Link to='/sign-in'><li className='sm:inline text-blue-950 hover:text-slate-400'>Sign in</li></Link>
+      <Link to='/'><li className='font-sans hidden sm:inline text-blue-950 hover:font-bold hover:text-white'>Home</li></Link>
+      <Link to='/about'><li className='hidden sm:inline text-blue-950 hover:text-white hover:font-bold' >About</li></Link>
+      <Link to='/sign-in'><li className='sm:inline text-blue-950 hover:text-white hover:font-bold'>Sign in</li></Link>
       </ul>
     </div>
     </header>

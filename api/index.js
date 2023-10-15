cfabd4c3c249eb9b0e1e34e7826395893d731516
lefline <issamkharbache2@gmail.com>
2023-10-15 12:04:01 +0100

    create middleware to handle errors

--- api/index.js
+++ api/index.js
@@ -23,6 +23,22 @@ app.use(express.json())
 app.listen(3000,() =>{
     console.log('Server is running on port 3000 ');
 });
+//user api router
 
 app.use('/api/user',userRouter);
+
+//authentifaction api router
 app.use('/api/auth',authRouter);
+
+//creating the middleware
+
+app.use((err,req,res,next)=>{
+const statusCode = err.statusCode || 500;
+const message = err.message || 'Internal server Error';
+return res.status(statusCode).json({
+    success :false,
+    statusCode,
+    message,
+
+});
+});

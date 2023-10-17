bc266cf48864bf97dcc081bf45772bbb535acf3a
Lefline <issamkharbache2@gmail.com>
2023-10-17 09:33:50 +0100

    completing signin functionalities

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link,useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+  const [formData,setFormData] = useState({});
+  const [error,setError] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData(prevValue => ({ ...prevValue,  [e.target.id]: e.target.value}));
+  }   
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      const res = await fetch('api/auth/signin',{
+        method: "POST",
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false){
+        setError(data.message);
+        setLoading(false);
+        return;
+  
+      }setLoading(false);
+      setError(null);
+      navigate('/');
+     
+    } catch (error) {
+      setLoading(false);
+    }
+   
+
+  }
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4 shadow-sm'>
+        <input type="email" placeholder='Email here' className='border p-3 rounded-lg' id="email"   onChange={handleChange}/>
+        <input type="password" placeholder='Password here here' className='border p-3 rounded-lg' id="password" onChange={handleChange} />
+        <button disabled ={loading}  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-75 disabled:opacity-40' >{loading ? "Loading..." : "Sign in"}</button>
+      </form>
+      {error && <p className='text-red-500'>{error}</p>}
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account ?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700 font-bold'>Sign up</span>
+        </Link>
+
+      </div>
+      
+    </div>
   )
-}
+} 
